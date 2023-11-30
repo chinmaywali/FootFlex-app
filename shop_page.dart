cd532148b23928db19bef8b5475f37a50f513fd7
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-11-30 12:59:04 +0530

    Create shop_page.dart

---
+++ shop_page.dart
@@ -0,0 +1,117 @@
+import 'package:ecomm_app/cart.dart';
+import 'package:ecomm_app/shoe.dart';
+import 'package:ecomm_app/shoe_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  //add shoe to cart
+  void addShoeToCart(Shoe shoe) {
+    Provider.of<Cart>(context, listen: false).addItemToCart(shoe);
+
+    //alert user
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Successfully Added!"),
+        content: Text("Check Your Cart"),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        //search Bar
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+          ),
+          //message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'Everyone flies.., but some fly longer than others',
+              style: TextStyle(color: Colors.grey[900]),
+            ),
+          ),
+          //hot picks
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hot Picks🔥',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+
+          //list of shoes for sale
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                Shoe shoe = value.getShoeList()[index];
+
+                return ShoeTile(
+                  shoe: shoe,
+                  onTap: () => addShoeToCart(shoe),
+                ); //still error
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 25, right: 25),
+            child: Divider(
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
