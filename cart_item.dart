3e0225b6c9929e154c7c42c4a879b2f21810ff41
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-11-30 13:05:33 +0530

    Create cart_item.dart

---
+++ cart_item.dart
@@ -0,0 +1,59 @@
+import 'package:ecomm_app/cart.dart';
+import 'package:ecomm_app/shoe.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartItem extends StatefulWidget {
+  Shoe shoe;
+  CartItem({
+    super.key,
+    required this.shoe,
+  });
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  //remove item from cart
+  void removeItemFromCart() {
+    Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: Image.asset(widget.shoe.imagePath),
+        title: Text(widget.shoe.name),
+        subtitle: Text('Rs.' + widget.shoe.price),
+        trailing: IconButton(
+            onPressed: removeItemFromCart,
+            icon: Icon(
+              Icons.delete,
+              color: Colors.red,
+            )),
+      ),
+    );
+  }
+}
