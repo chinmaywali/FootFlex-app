987b4ba7411121a3e003e63fdf157eacee7e785a
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-11-30 12:55:52 +0530

    Create bottom_nav_bar.dart

---
+++ bottom_nav_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: GNav(
+          color: Colors.grey[400], //for unselected
+          activeColor: Colors.grey.shade700, //for selected
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 16,
+          gap: 8, //tab border radius
+          onTabChange: (Value) => onTabChange!(Value),
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Shop',
+            ),
+            GButton(
+              icon: Icons.shopping_bag_rounded,
+              text: 'Cart',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
